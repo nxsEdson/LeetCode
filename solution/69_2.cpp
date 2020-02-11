ff977a24048b50968464601bf75e2c927d470f09
Edson-Niu <nxs583966@163.com>
2020-02-11 12:12:57 +0800

    Create 69_2.cpp

---
+++ solution/69_2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    if (x <= 1)
+        return x;
+    int left = 1, right = x;
+    while (true) 
+    {
+        int mid = left + (right - left)/2; // for INT_MAX
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+    }
+};
